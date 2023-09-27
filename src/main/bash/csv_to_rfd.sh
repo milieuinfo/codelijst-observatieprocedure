547f8cd0c2a9fb027d1f7285379ddd85753017a0
gezever <gezever@skynet.be>
2023-09-27 16:49:44 +0200

    fix #1

--- src/main/bash/csv_to_rfd.sh
+++ src/main/bash/csv_to_rfd.sh
@@ -3,3 +3,5 @@
 # Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/observatieprocedure/observatieprocedure.csv
 # to jsonld, /tmp/observatieprocedure.jsonld
 Rscript ../R/csv_to_json.R
+shacl v --shapes ../resources/be/vlaanderen/omgeving/data/id/ontology/observatieprocedure-ap-constraints/observatieprocedure-ap-constraints.ttl --data ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/observatieprocedure/observatieprocedure.ttl
+
